e0c8a6b2dfb23d2c767916471a48fdca58b77469
Versa-Sketch <gigforgig2023@gmail.com>
2026-08-25 11:09:04 +0530

    changes

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    ]
+
+    title = models.CharField(max_length = 200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    
+    status = models.CharField(max_length = 10, choices=STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return self.title
+
+
